3c7e5f8e0ef072f1b8dbe686cc1953b0c147230a
Ethan <ethan@zolplay.com>
2023-11-16 16:22:22 +0800

    refactor: docs to le-ai

---
+++ src/components/home/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div className="mx-auto bg-fuchsia-50 h-[2000px] max-w-7xl">as2fasf</div>
+  );
+}
--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,62 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import React from "react";
+import Image from "next/image";
+import { DocsThemeConfig } from "nextra-theme-docs";
+
+const logo = (
+  <div className="flex h-14 text-2xl gap-2 items-center logo">
+    <Image
+      className="cursor-pointer"
+      src="/logo.png"
+      alt="logo"
+      width={30}
+      height={30}
+    />
+    <div className="cursor-pointer flex font-extrabold text-transparent items-center select-none">
+      <span className="bg-clip-text bg-logo bg-[size:400%] animate-flow">
+        Le-AI
+      </span>
+    </div>
+    <style jsx>{`
+      .logo {
+        mask-image: linear-gradient(
+          60deg,
+          black 25%,
+          rgba(0, 0, 0, 0.2) 50%,
+          black 75%
+        );
+        mask-size: 400%;
+        mask-position: 0%;
+      }
+      .logo:hover {
+        mask-position: 100%;
+        transition: mask-position 1s ease, -webkit-mask-position 1s ease;
+      }
+    `}</style>
+  </div>
+);
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: "https://github.com/LTopx/Le-AI",
   },
   chat: {
-    link: 'https://discord.com',
+    link: "https://discord.com",
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  // main: ({ children }) => {
+  //   return <div className="bg-fuchsia-300">{children}</div>;
+  // },
+  // useNextSeoProps() {
+  //   const { asPath } = useRouter();
+  //   if (asPath !== "/") {
+  //     return {
+  //       titleTemplate: "%s – LX-UI",
+  //     };
+  //   }
+  // },
   footer: {
-    text: 'Nextra Docs Template',
+    text: "Nextra Docs Template",
   },
-}
+};
 
-export default config
+export default config;
